2f755d454f6336d702c20eb51eb438fa8fc7eafa
Mayank Rajput (HackElite) <78653024+hackelite01@users.noreply.github.com>
2023-06-15 15:21:09 +0530

    Update index.jsx

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -92,7 +92,7 @@ const Home = ({ user }) => {
     if (!isDone && subLists?.length && status === 0) {
       addStatus()
     }
-  }, [completed, subLists, isDone, status])
+  }, [completed, subLists, isDone])
 
   return (
     <>
